9d3df0e6c114f1e72b8ec49c8e085e08968c777d
Kent Richards <kent-richards@outlook.com>
2023-12-30 10:56:52 -0400

    fixed issue with eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,10 @@
-export default {
+module.exports = {
     env: {
         browser: true,
         es2021: true,
         node: true,
     },
     extends: ['plugin:prettier/recommended', 'prettier', 'eslint:recommended'],
-    plugins: ['@typescript-eslint'],
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
