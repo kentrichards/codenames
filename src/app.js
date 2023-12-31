a53e7ea818dd3bcf0c1fea5ce655a30b72a1516a
Ava Powelson <av596233@dal.ca>
2023-12-30 20:18:33 -0800

    using sockets instead of router

--- src/app.js
+++ src/app.js
@@ -4,22 +4,72 @@ import { fileURLToPath } from 'url'
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
-
-import router from './router.js'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
 const port = process.env.port || 3000
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
+const activeRooms = []
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(cookieParser())
 app.use(logger('dev'))
-app.use('/', router)
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+app.get('/', (req, res) => {
+    res.render('index')
 })
+
+io.on('connection', (socket) => {
+    socket.join("main")
+    console.log('a user connected to main');
+    
+    socket.on('createRoom', (roomCode) => {
+        const newRoom = {
+            roomCode: roomCode,
+            players: [],
+            gameState: {},
+        }
+        activeRooms.push(newRoom)
+
+        socket.join(roomCode)
+        socket.leave('main')
+        console.log(socket.rooms)
+    })
+
+    socket.on('joinRoom', (roomCode) => {
+        // Also have to:
+            // add player to players list of appropriate room
+            // check that room exists
+            socket.join(roomCode)
+            socket.leave('main')
+
+        console.log(socket.rooms)
+    })
+
+    socket.on('showGames', () => {
+        // Show all rooms
+        const rooms = io.of("/").adapter.rooms;
+        console.log(rooms)
+        io.emit('rooms', rooms)
+    })
+    
+    socket.on('msgRm', (room) => {
+        io.to(room).emit('rmMsg', `Hello to room ${room}`)
+    })
+
+    socket.on('disconnect', reason => {
+        console.log(`a user disconnected because of ${reason}`);
+    });
+});
+
+server.listen(port, () => {
+    console.log('server running at http://localhost:3000');
+});
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -2,27 +2,64 @@ const usernameInput = /** @type {HTMLInputElement} */ (document.getElementById('
 const createGameBtn = /** @type {HTMLButtonElement} */ (document.getElementById('create-game'))
 const roomCodeInput = /** @type {HTMLInputElement} */ (document.getElementById('room-code'))
 const joinGameBtn = /** @type {HTMLButtonElement} */ (document.getElementById('join-game'))
+const showGamesBtn = /** @type {HTMLButtonElement} */ (document.getElementById('show-games'))
+const msgRmBtn = /** @type {HTMLButtonElement} */ (document.getElementById('msg-rm'))
+const infoMsg = /** @type {HTMLParagraphElement} */ (document.getElementById('msg'))
+const socket = io()
+
+var myRoom = ''
+
+socket.on('rmMsg', (msg) => {
+    infoMsg.innerText = msg
+})
+
+function generateRoomCode() {
+    // todo make better
+    return Math.floor(Math.random() * 1000).toString()
+}
 
 createGameBtn.addEventListener('click', (/** @type {MouseEvent} */ ev) => {
     ev.preventDefault()
 
     const username = usernameInput.value
+    const roomCode = generateRoomCode()
+    myRoom = roomCode
+
     if (!username) {
         console.error('Username required to create game')
         return
     }
 
     document.cookie = `username=${encodeURIComponent(username)}; SameSite=Strict`
+    infoMsg.innerText = "You are now a part of room " + roomCode
+    socket.emit("createRoom", roomCode)
+})
 
-    fetch('/createGame')
-        .then(res => {
-            if (!res.ok) {
-                throw new Error(`status=${res.status}`)
-            } else if (res.redirected) {
-                window.location.href = res.url
-            }
-        })
-        .catch(err => {
-            console.error(`Error creating game: ${err}`)
-        })
+joinGameBtn.addEventListener('click', (/** @type {MouseEvent} */ ev) => {
+    ev.preventDefault()
+
+    const username = usernameInput.value
+    const roomCode = roomCodeInput.value
+    myRoom = roomCode
+
+    if (!username) {
+        console.error('Username required to create game')
+        return
+    }
+
+    document.cookie = `username=${encodeURIComponent(username)}; SameSite=Strict`
+    infoMsg.innerText = "You are now a part of room " + roomCode
+    socket.emit("joinRoom", roomCode)
+})
+
+// For Testing
+showGamesBtn.addEventListener('click', (/** @type {MouseEvent} */ ev) => {
+    ev.preventDefault()
+    socket.emit("showGames")
+})
+
+msgRmBtn.addEventListener('click', (/** @type {MouseEvent} */ ev) => {
+    ev.preventDefault()
+    console.log('sending message to room ' + myRoom)
+    socket.emit("msgRm", myRoom)
 })
