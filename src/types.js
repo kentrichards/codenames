618f954fb5c81051bb3a4c22bacd3300bf92a45c
Kent Richards <kent-richards@outlook.com>
2024-01-04 15:33:22 -0400

    Add Room and Player types

---
+++ src/types.js
@@ -0,0 +1,14 @@
+/**
+ * @typedef {Object} Room
+ * @property {string} roomCode
+ * @property {Player[]} players
+ * @property {{}} gameState
+ */
+
+/**
+ * @typedef {Object} Player
+ * @property {string} username
+ * @property {string} team
+ * @property {string} role
+ * @property {import('ws')} ws
+ */
