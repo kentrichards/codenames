5d63d25ae76823228cbce8e5d4a65e9bae4bc4ad
Ava Powelson <av596233@dal.ca>
2024-01-04 10:46:19 -0800

    timers work now. each room has one timer

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import express from 'express'
 import {
     activeRooms,
     createRoom,
-    closeRoom,
     getRoom,
     broadcast,
     redirectSocket,
@@ -12,23 +11,16 @@ import {
 
 const router = express.Router()
 
-const idleTime = { timer: 0 }
-let emptyRoomTimeout = null
-let idleTimer = null
-
 // eslint-disable-next-line no-unused-vars
 export default expressWsInstance => {
     router.get('/', (req, res) => {
-        if (idleTimer != null) {
-            clearInterval(idleTimer)
-        }
         res.render('index')
     })
 
     router.get('/createGame', (req, res) => {
         const newRoom = createRoom()
+        newRoom.gameState.idleTimer = setInterval(idleTimeout, 60000, newRoom)
         activeRooms.push(newRoom)
-        idleTimer = setInterval(idleTimeout, 60000, newRoom, idleTime)
         res.redirect(`/${newRoom.roomCode}`)
     })
 
@@ -65,27 +57,10 @@ export default expressWsInstance => {
         const username = req.cookies.username
         const room = getRoom(roomCode)
 
-        // Is this necessary when we already check if the room exists in .get('/:roomCode')?
-        // Wondering if there is a race condition we need to worry about w.r.t timers closing rooms
-        if (!room) {
-            redirectSocket(ws, '/')
-            return
-        }
-
         ws.on('message', (/** @type {String} */ msg) => {
             const action = JSON.parse(msg)
 
-            // This is not strictly necessary, the below if-else chain will catch this case without issue
-            // If !action.type, action.type === undefined, so we will be comparing undefined to strings below
-            // and will hit the else clause (which prints a similar error message)
-            // We only run into problems if we try to access a property on another, undefined, property
-            // E.g. accessing action.type.other without checking if action.type exists
-            if (!action.type) {
-                console.error(`Bad message received: ${msg}`)
-                return
-            }
-
-            idleTime.timer = 0
+            room.gameState.idleTime = 0
 
             if (action.type === 'userConnected') {
                 const newPlayer = {
@@ -97,25 +72,19 @@ export default expressWsInstance => {
 
                 room.players.push(newPlayer)
 
-                if (emptyRoomTimeout != null) {
-                    // clearTimeout is a Node function — should this be clearInterval?
-                    clearTimeout(emptyRoomTimeout)
-                }
-
                 broadcast(room, `User ${username} joined room ${roomCode}`)
             } else if (action.type === 'message') {
                 broadcast(room, `${username}: ${action.payload}`)
             } else {
                 console.error(`Unknown message received: ${msg}`)
             }
+
         })
 
         ws.on('close', () => {
+            room.gameState.idleTime = 0
             broadcast(room, `User ${username} left the room`)
             removePlayer(room, ws)
-            if (room.players.length < 1) {
-                emptyRoomTimeout = setInterval(closeRoom, 120000, room)
-            }
         })
     })
 
